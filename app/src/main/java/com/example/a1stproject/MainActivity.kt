34b75a3a864ccb0461055bbe95aedfbd29f5d826
resanti2004 <resantidwicahyani@mail.ugm.ac.id>
2023-08-29 09:59:25 +0700

    oke

--- app/src/main/java/com/example/a1stproject/MainActivity.kt
+++ app/src/main/java/com/example/a1stproject/MainActivity.kt
@@ -5,15 +5,33 @@ import android.os.Bundle
 import android.widget.Toast
 import com.example.a1stproject.databinding.ActivityMainBinding
 
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         var binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        with(binding) {
+//        with(binding) {
+//            btnTest1.setOnClickListener{
+//                Toast.makeText(this@MainActivity, "Hallo Teman Teman", Toast.LENGTH_SHORT).show()
+//            }
+//        }
+
+        with(binding  ){
             btnTest1.setOnClickListener{
-                Toast.makeText(this@MainActivity, "Hallo Teman Teman", Toast.LENGTH_SHORT).show()
+                //Mengambil teks dari EditText untuk username
+                val username = username.text.toString()
+                //Memeriksa apakah bidang username tidak kosong
+                if (username.isNotEmpty()) {
+                    //Membuat pesan dengan nama pengguna yang berhasil login
+                    val message = "Hallo, $username berhasil login!"
+                    //Menampilkan pesan Toast dengan pesan yang telah dibuat
+                    Toast.makeText(this@MainActivity, message, Toast.LENGTH_LONG).show()
+                } else {
+                    //Jika bidang username kosong, tampilkan pesan Toast bahwa username belum diisi
+                    Toast.makeText(this@MainActivity, "Username belum diisi", Toast.LENGTH_SHORT).show()
+                }
             }
         }
     }
